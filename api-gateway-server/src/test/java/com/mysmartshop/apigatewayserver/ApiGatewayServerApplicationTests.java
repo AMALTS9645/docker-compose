5f5e44c72aca59d3655feb87dbe2ce08bd80ce82
AMAL T S <amaltsthekkel@gmail.com>
2023-07-26 12:11:55 +0530

    gateway server

---
+++ api-gateway-server/src/test/java/com/mysmartshop/apigatewayserver/ApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysmartshop.apigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
